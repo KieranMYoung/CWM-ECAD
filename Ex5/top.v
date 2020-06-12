b82688c868ead2b281e794549dc1a362187eee10
Cloud User <centos@ip-172-31-13-61.eu-west-2.compute.internal>
2020-06-12 11:31:31 +0000

    Ex5 attempt

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 
-// Student Name:
-// Date: 
+// Student Name:Kieran Young
+// Date: 11-06-2020
 //
 //  Description: In this exercise, you need to implement a UK traffic lights 
 //  sequencing system. 
@@ -12,4 +12,24 @@
 //  outputs:
 //           red, amber, green
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps 
 
+module TrafficLights(clk, red, amber, green);
+
+// Ports
+input clk;
+
+output red;
+output amber;
+output green;
+
+// Registers & Wires
+
+// Logic                      //red -> red/amber -> green -> amber -> red...
+always @ (posedge clk)
+  if (red && !amber && !green)
+    assign red <= 1;
+    assign amber <= 1;
+    assign green <= 0;
+
+endmodule
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -1,9 +1,48 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #5 - Traffic Lights
-// Student Name:
-// Date: 
+// Student Name:Kieran Young
+// Date: 12-06-2020
 //
 // Description: A testbench module to test Ex5 - Traffic Lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps 
 
+module TrafficLights_tb(
+);
+
+// Parameters
+parameter clk_period = 10;
+
+// Registers & Wires
+reg clk;
+reg red;
+reg amber;
+reg green;
+
+// Clock generation
+    initial begin 
+    clk = 1'b0;
+    forever 
+    #(clk_period/2) clk = ~clk;
+    end
+
+// Logic 
+initial begin
+    red = 1;
+    amber = 0;
+    green = 0; 
+end
+
+// Finish test, check for success
+//    initial begin
+//        #(clk_period * 50);
+//        if (err==0)
+//          $display("***TEST PASSED! :) ***");
+//        $finish;
+//   end
+
+// Instantiate module
+    TrafficLights lights(clk, red, amber, green);
+
+endmodule 
